48958939505dd70f5349f0a4d666f2b115efe85b
Naim Salameh <naim.salameh@arm.com>
2017-12-11 15:27:39 +0200

    first try for server container, container working, need to check networking

--- server.py
+++ server.py
@@ -2,7 +2,10 @@
 import web
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('user_data.xml')
+from pathlib import Path
+
+xmlfile = Path('./src/user_data.xml')
+tree = ET.parse(str(xmlfile))
 root = tree.getroot()
 
 urls = (
@@ -12,20 +15,23 @@ urls = (
 
 app = web.application(urls, globals())
 
-class list_users:        
+
+class list_users:
     def GET(self):
-	output = 'users:[';
-	for child in root:
-                print 'child', child.tag, child.attrib
-                output += str(child.attrib) + ','
-	output += ']';
+        output = 'users:['
+        for child in root:
+            print('child', child.tag, child.attrib)
+            output += str(child.attrib) + ','
+        output += ']'
         return output
+
 
 class get_user:
     def GET(self, user):
-	for child in root:
-		if child.attrib['id'] == user:
-		    return str(child.attrib)
+        for child in root:
+            if child.attrib['id'] == user:
+                return str(child.attrib)
+
 
 if __name__ == "__main__":
     app.run()
